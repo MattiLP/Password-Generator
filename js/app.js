c9c8524462f63c5483652aadff8cac488c09df88
Nosthy <83171237+Nosthy@users.noreply.github.com>
2021-05-19 15:27:06 +0200

    Added control of password length

--- js/app.js
+++ js/app.js
@@ -2,38 +2,40 @@ const passwordDisplay = document.getElementById('password-display');
 const useUpercaseCheckbox = document.getElementById('use-upercase-checkbox');
 const useSpecialCharactersCheckbox = document.getElementById('use-special-characters-checkbox');
 const useDigitsCheckbox = document.getElementById('use-digits-checkbox');
+const passwordLength = document.getElementById('password-length');
 
 document.getElementById('genrate-button').addEventListener('click', () => generate());
 useUpercaseCheckbox.addEventListener('click', () => generate());
 useSpecialCharactersCheckbox.addEventListener('click', () => generate());
 useDigitsCheckbox.addEventListener('click', () => generate());
 
+passwordLength.value = 10;
+useUpercaseCheckbox.checked = true;
+useDigitsCheckbox.checked = true;
+
 generate();
 
 function generate() {
     var password = '';
-    var currentCharacters = [];
+    var currentChars = [];
 
-    for(var i = 0; i < 10; i++)
-    {
-        currentCharacters.push(lettersLowercase[Math.floor(Math.random() * lettersLowercase.length)]);
+    for(var i = 0; i < Math.round(passwordLength.value); i++) {
+        currentChars.push(lettersLowercase[Math.round(Math.random() * (lettersLowercase.length - 1))]);
 
-        if(useDigitsCheckbox.checked)
-        {
-            currentCharacters.push(digits[Math.floor(Math.random() * digits.length)]);
+        if(useDigitsCheckbox.checked) {
+            currentChars.push(digits[Math.round(Math.random() * (digits.length - 1))]);
         }
 
-        if(useUpercaseCheckbox.checked)
-        {
-            currentCharacters.push(lettersUpercase[Math.floor(Math.random() * lettersUpercase.length)]);
+        if(useUpercaseCheckbox.checked) {
+            currentChars.push(lettersUpercase[Math.round(Math.random() * (lettersUpercase.length - 1))]);
         }
 
-        if(useSpecialCharactersCheckbox.checked)
-        {
-            currentCharacters.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
+        if(useSpecialCharactersCheckbox.checked) {
+            currentChars.push(specialCharacters[Math.round(Math.random() * (specialCharacters.length - 1))]);
         }
 
-        password += currentCharacters[Math.floor(Math.random() * currentCharacters.length)];
+        password += currentChars[Math.round(Math.random() * (currentChars.length - 1))];  
+        currentChars = [];
     }
 
     passwordDisplay.innerHTML = password;
